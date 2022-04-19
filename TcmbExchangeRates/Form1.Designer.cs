1e2213d70832a66f5fc1e91d762a0162c89bf0d5
Hüseyin YILMAZ <hyilmaz07@outlook.com>
2022-04-19 18:24:30 +0300

    Add project files.

---
+++ TcmbExchangeRates/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace TcmbExchangeRates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnGetExchanges = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(49, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btnGetExchanges
+            // 
+            this.btnGetExchanges.Location = new System.Drawing.Point(249, 27);
+            this.btnGetExchanges.Name = "btnGetExchanges";
+            this.btnGetExchanges.Size = new System.Drawing.Size(75, 23);
+            this.btnGetExchanges.TabIndex = 1;
+            this.btnGetExchanges.Text = "Getir";
+            this.btnGetExchanges.UseVisualStyleBackColor = true;
+            this.btnGetExchanges.Click += new System.EventHandler(this.btnGetExchanges_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tarih :";
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Enabled = false;
+            this.btnTransfer.Location = new System.Drawing.Point(330, 27);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(75, 23);
+            this.btnTransfer.TabIndex = 3;
+            this.btnTransfer.Text = "Aktar";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(410, 231);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 178);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(416, 250);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kurlar";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.btnTransfer);
+            this.groupBox2.Controls.Add(this.btnGetExchanges);
+            this.groupBox2.Location = new System.Drawing.Point(12, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(416, 65);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Genel";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnConnect);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.txtPass);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.txtDatabase);
+            this.groupBox3.Controls.Add(this.txtUser);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtServer);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(416, 89);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mikro";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(330, 17);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 56);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Bağlan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Pass :";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(224, 53);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 3;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(159, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Database :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User :";
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(224, 18);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(100, 20);
+            this.txtDatabase.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(56, 53);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Server :";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(56, 18);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 20);
+            this.txtServer.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.progressBar1);
+            this.groupBox4.Location = new System.Drawing.Point(15, 434);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(413, 37);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 10);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(401, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 483);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnGetExchanges;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ TcmbExchangeRates/Form1.cs
@@ -0,0 +1,188 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcmbExchangeRates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        SqlConnection con = new SqlConnection();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void SetControls()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                txtServer.Enabled = false;
+                txtDatabase.Enabled = false;
+                txtUser.Enabled = false;
+                txtPass.Enabled = false;
+                btnConnect.Text = "Bağlantıyı kes";
+                btnTransfer.Enabled = true;
+            }
+            else
+            {
+                txtServer.Enabled = true;
+                txtDatabase.Enabled = true;
+                txtUser.Enabled = true;
+                txtPass.Enabled = true;
+                btnConnect.Text = "Bağlan";
+                btnTransfer.Enabled = false;
+            }
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                else
+                {
+                    con.ConnectionString = $@"Server={txtServer.Text};Database={txtDatabase.Text};User ID={txtUser.Text};Password={txtPass.Text};Trusted_Connection=False;";
+                    con.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı Kurulamadı. \n" + ex.Message);
+            }
+            SetControls();
+        }
+
+        private void btnGetExchanges_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Exchange.GetDataTableAllCurrenciesHistoricalExchangeRates(dateTimePicker1.Value);
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(Transfer));
+            t.Start();
+
+        }
+        class KurIsimleri
+        {
+            public string Kur_sembol { get; set; }
+            public int Kur_No { get; set; }
+        }
+        void Transfer()
+        {
+            progressBar1.Value = 0;
+            progressBar1.Maximum = dataGridView1.Rows.Count;
+            string kurIsimleriQuery = "SELECT Kur_No,Kur_sembol  FROM  KUR_ISIMLERI";
+            List<KurIsimleri> kurIsimleriList;
+     
+            string maxRecNoQuery = "select MAX(dov_RECid_RECno) from DOVIZ_KURLARI";
+            int MaxRecNo = 0;
+            string InsertQuery = @"INSERT INTO DOVIZ_KURLARI
+           (dov_RECid_DBCno
+           ,dov_RECid_RECno
+           ,dov_SpecRecNo
+           ,dov_iptal
+           ,dov_fileid
+           ,dov_hidden
+           ,dov_kilitli
+           ,dov_degisti
+           ,dov_checksum
+           ,dov_create_user
+           ,dov_create_date
+           ,dov_lastup_user
+           ,dov_lastup_date
+           ,dov_special1
+           ,dov_special2
+           ,dov_special3
+           ,dov_no
+           ,dov_tarih
+           ,dov_fiyat1
+           ,dov_fiyat2
+           ,dov_fiyat3
+           ,dov_fiyat4)
+     VALUES
+           (@dov_RECid_DBCno
+           ,@dov_RECid_RECno
+           ,@dov_SpecRecNo
+           ,@dov_iptal
+           ,@dov_fileid
+           ,@dov_hidden
+           ,@dov_kilitli
+           ,@dov_degisti
+           ,@dov_checksum
+           ,@dov_create_user
+           ,@dov_create_date
+           ,@dov_lastup_user
+           ,@dov_lastup_date
+           ,@dov_special1
+           ,@dov_special2
+           ,@dov_special3
+           ,@dov_no
+           ,@dov_tarih
+           ,@dov_fiyat1
+           ,@dov_fiyat2
+           ,@dov_fiyat3
+           ,@dov_fiyat4)";
+
+            using (var connection = con)
+            {
+                kurIsimleriList = connection.Query<KurIsimleri>(kurIsimleriQuery).ToList();
+                MaxRecNo = con.QueryFirstOrDefault<int>(maxRecNoQuery);
+                MaxRecNo = MaxRecNo+1;
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DynamicParameters p = new DynamicParameters();
+                    p.Add("dov_RECid_DBCno", 0);
+                    p.Add("dov_RECid_RECno", MaxRecNo + i);
+                    p.Add("dov_SpecRecNo", 0);
+                    p.Add("dov_iptal", false);
+                    p.Add("dov_fileid", 1007);
+                    p.Add("dov_hidden", false);
+                    p.Add("dov_kilitli", false);
+                    p.Add("dov_degisti", false);
+                    p.Add("dov_checksum", 0);
+                    p.Add("dov_create_user", 2);
+                    p.Add("dov_create_date", DateTime.Now);
+                    p.Add("dov_lastup_user", 2);
+                    p.Add("dov_lastup_date", DateTime.Now);
+                    p.Add("dov_special1", "");
+                    p.Add("dov_special2", "");
+                    p.Add("dov_special3", "");
+                    p.Add("dov_no", kurIsimleriList.Where(a => a.Kur_sembol.Equals(dataGridView1.Rows[i].Cells[1].Value.ToString())).SingleOrDefault().Kur_No);
+                    p.Add("dov_tarih", dateTimePicker1.Value.Date);
+                    p.Add("dov_fiyat1", Convert.ToDecimal(dataGridView1.Rows[i].Cells[3].Value));
+                    p.Add("dov_fiyat2", Convert.ToDecimal(dataGridView1.Rows[i].Cells[4].Value));
+                    p.Add("dov_fiyat3", Convert.ToDecimal(dataGridView1.Rows[i].Cells[5].Value));
+                    p.Add("dov_fiyat4", Convert.ToDecimal(dataGridView1.Rows[i].Cells[6].Value));
+                    con.Query(InsertQuery, p);
+                    progressBar1.Value = i + 1;
+                }
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dataGridView1.Rows[0].Cells[2].Value.ToString());
+        }
+    }
+}
